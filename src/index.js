3f13c9a9a2af29ba655225e84768e3e1ae2e8d6a
Vitaliy <you@example.com>
2020-10-03 20:54:47 +0300

    feat: add method max element

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ exports.min = function min(array) {
     else if (!array) {
         return 0;
     } else {
-    result = Math.min.apply(null, array);
-    return result;
+        result = Math.min.apply(null, array);
+        return result;
     }
 }
 
 exports.max = function max(array) {
-
-    return 0;
+    let result = 0;
+    if (array == '') {
+        return 0;
+    }
+    else if (!array) {
+        return 0;
+    } else {
+        result = Math.max.apply(null, array);
+        return result;
+    }
 }
 
 exports.avg = function avg(array) {
