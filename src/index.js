13813491a74fea57f36952b09b840f79f376c412
Vitaliy <you@example.com>
2020-10-03 20:43:30 +0300

    feat: add method min element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let result = 0;
+    if (array == '') {
+        return 0;
+    }
+    else if (!array) {
+        return 0;
+    } else {
+    result = Math.min.apply(null, array);
+    return result;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return 0;
 }
